9dbc2e681ac2b9bc5188f34b1c38898a43dc4407
wallef <wallefsousa1@gmail.com>
2019-03-11 20:45:53 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+</head>
+
+<body>
+
+	<div class="signup-form">
+		
+		<form class="" action="index.html" method="post">
+			<p>Registre-se Abaixo</p>
+			<h1>Login</h1>
+
+			<input type="text" name="" placeholder="Nome completo" class="txtb">
+			<input type="email" name="" placeholder="Email" class="txtb">
+			<input type="password" name="" placeholder="Senha" class="txtb">
+
+			<input type="submit" value="Entrar" class="signup-btn">
+			<a href="#">Ainda não tem um ? </a>
+
+
+		</form>
+
+
+	</div>
+
+
+
+</body>
+</html>
